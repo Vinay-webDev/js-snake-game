95f82d0657b93d59a1256c014b09bbb3c99a704b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-18 23:42:43 +0530

    canvas🐍

---
+++ js/snake.js
@@ -0,0 +1,31 @@
+// snake
+var blockSize = 25;
+var rows = 20;
+var columns = 20;
+var board;
+var context;
+
+window.onload = function () {
+    const board = document.getElementById("board");
+    board.width = rows * blockSize;
+    board.height = rows * blockSize;
+    context = board.getContext("2d"); // used to draw on the board
+    update();
+}
+
+function update () {
+    context.fillStyle = "black"; // this will set the pen color to black
+    context.fillRect(0, 0, board.width, board.height);
+}
+
+
+
+
+
+
+
+
+
+
+
+

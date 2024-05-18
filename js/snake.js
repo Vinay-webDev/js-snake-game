0709b19c9ad0c7c19a8008a631cee13ee04c088f
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-19 00:21:10 +0530

    food🍕 and snake🐍

--- js/snake.js
+++ js/snake.js
@@ -1,21 +1,41 @@
-// snake
+// board
 var blockSize = 25;
 var rows = 20;
-var columns = 10;
+var columns = 20;
 var board;
 var context;
 
+// snake🐍
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+// food🍕
+var foodX = blockSize * 10;
+var foodY = blockSize * 10;
+
 window.onload = function () {
     board = document.getElementById("board");
-    board.width = rows * blockSize;
     board.height = rows * blockSize;
+    board.width = columns * blockSize;
     context = board.getContext("2d"); // used to draw on the board
+    placeFood();
     update();
+}
+// to draw food at random position
+function placeFood() {
+    foodX = Math.floor(Math.random()* columns) * blockSize;
+    foodY = Math.floor(Math.random()* rows) * blockSize;
 }
 
 function update () {
     context.fillStyle = "black"; // this will set the pen color to black
     context.fillRect(0, 0, board.width, board.height);
+    //to draw snake🐍
+    context.fillStyle = "lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    // to draw food🍕
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
 }
 
 
